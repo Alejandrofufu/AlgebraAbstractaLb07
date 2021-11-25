7976f8a2b786c4eae781992d5df32b70f0f4e573
Alejandro <borsal_ino@hotmail.com>
2021-11-25 14:51:59 -0500

    Lab07 pregunta1

--- main.cpp
+++ main.cpp
@@ -2,34 +2,33 @@
 #include<math.h>
 using namespace std;
 
-long long EuclidesGeneral(long long a, long long b, long long& x, long long& y)
+ long long  EuclidesGeneral( long long  a,  long long  b,  long long  & x,  long long  & y)
 {
     if (a == 0)
     {
         x = 0, y = 1;
         return b;
     }
-    long long x1, y1;
-    long long gcd = EuclidesGeneral(b % a, a, x1, y1);
+     long long  x1, y1;
+     long long  gcd = EuclidesGeneral(b % a, a, x1, y1);
     x = y1 - (b / a) * x1;
     y = x1;
-
     return gcd;
 }
 
-long long inversoM(long long a, long long m) {
-    long long x = 0, y = 0;
-    long long g = EuclidesGeneral(a, m, x, y);
+ long long  inversoM( long long int a,  long long int m) {
+     long long  x = 0, y = 0;
+     long long  g = EuclidesGeneral(a, m, x, y);
     if (g != 1)
         return -1;
     else {
-        long long res = (x % m + m) % m;
+         long long int res = (x % m + m) % m;
         return res;
     }
 }
 
-long long Gcd(long long a, long long b) {
-    long long t;
+ long long int Gcd( long long int a,  long long int b) {
+     long long int t;
     while (true) {
         t = a % b;
         if (t == 0)
@@ -39,10 +38,10 @@ long long Gcd(long long a, long long b) {
     }
 }
 
-void NAIVE_RSA_KEY_GENERATOR(long long p, long long q, long long& n, long long& e, long long& d) {
+void NAIVE_RSA_KEY_GENERATOR( long long int p,  long long int q,  long long int & n,  long long int& e,  long long int& d) {
     n = p * q;
-    long long phi = (p - 1) * (q - 1);
-    long long pista = 0;
+     long long int phi = (p - 1) * (q - 1);
+     long long int pista = 0;
     while (e < phi) { // e = {2,(p - 1) * (q - 1)}
         pista = Gcd(e, phi);
         if (pista == 1) {
@@ -53,34 +52,46 @@ void NAIVE_RSA_KEY_GENERATOR(long long p, long long q, long long& n, long long& 
             e++;
     }
 }
+long long int modular(unsigned long long int base, unsigned long long int exp, unsigned long long int mod)
+{
+    long int x = 1;
+    int i;
+    long long int power = base % mod;
+
+    for (i = 0; i < sizeof(int) * 64; i++) {
+        int least_sig_bit = 0x00000001 & (exp >> i);
+        if (least_sig_bit)
+            x = (x * power) % mod;
+        power = (power * power) % mod;
+    }
+
+    return x;
+}
 
 int main() {
-    long long mensaje = 0, p = 3, q = 11, n = 0, e = 2, d = 0, Pm = 0, Sc = 0;
+     long long int mensaje = 0, p = 3, q = 11, n = 0, e = 2, d = 0, Pm = 0, Sc = 0;
     cout << "Tabla: " << "m" << " ^ " << "e" << " mod " << "n" << " = " << " c \n";
     while (mensaje < p * q) {
         NAIVE_RSA_KEY_GENERATOR(p, q, n, e, d);
-        Pm = pow(mensaje, e);
-        Pm = (Pm % n + n) % n;// mensaje cifrado
-        Sc = pow(Pm, d);
-        Sc = (Sc % n + n) % n;// mensaje descifrado
-        cout << "\t\t" << mensaje << " ^ " << e << " mod " << n << " = " << Pm << "\t\tmensaje descifrado: " << Sc << "\n";
-        mensaje = mensaje + 1;
+        Pm = modular(mensaje,e,n); // mensaje cifrado  mensaje^e mod n
+        Sc = modular(Pm, d,n);// mensaje descifrado  c ^d mod n = mensaje 
+        cout <<d<< "\t\t" << mensaje << " ^ " << e << " mod " << n << " = " << Pm << "\t\tmensaje descifrado: " << Sc << "\n";
+        mensaje = mensaje +1;
     }
     /////////////////////////////////////
     mensaje = 0, p = 3, q = 11, n = 0, e = 2, d = 0, Pm = 0, Sc = 0;
     cout << "\n\nEscriba un mensaje m[0,32]: ";
     cin >> mensaje;
-    while (e < (p-1) * (q-1)) {
+    while (e < (p - 1) * (q - 1)) {
         NAIVE_RSA_KEY_GENERATOR(p, q, n, e, d);
-        Pm = pow(mensaje, e);
-        Pm = (Pm % n + n) % n;// mensaje cifrado
+        Pm = modular(mensaje, e, n);// mensaje cifrado
         if (Pm == mensaje) { // m ^ e mod n == m
-            Sc = pow(Pm, d);
-            Sc = (Sc % n + n) % n;// mensaje descifrado
-            cout <<"\t\t" << mensaje << " ^ " << e << " mod " << n << " = " << Pm << "\t\tmensaje descifrado: " << Sc << "\n";
+            Sc = modular(Pm, d, n);// mensaje descifrado
+            cout << "\t\t" << mensaje << " ^ " << e << " mod " << n << " = " << Pm << "\t\tmensaje descifrado: " << Sc << "\n";
         }
-        e = e+1;
-    }
+        e = e + 1;
 
+    }
+    
     return 0;
 }
